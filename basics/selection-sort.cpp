3a873288702b79a4f05789804f1ea301c59f7a02
Siddharth <178361631+u-s1ddhar7h@users.noreply.github.com>
2025-01-10 22:09:45 +0530

    selection sort of char data-type

--- basics/selection-sort.cpp
+++ basics/selection-sort.cpp
@@ -46,13 +46,33 @@ void selectSort(int arr[], int n) {
     }
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
+    cout << endl;
+    return;
+}
+void selectSortCharacter(char arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[index])
+                index = j;
+        }
+        std::swap(arr[i], arr[index]);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return;
 }
 int main() {
-    int arr[6] = {8, 4, 1, 10, 15, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    // int arr[6] = {8, 4, 1, 10, 15, 5};
+    // int size = sizeof(arr) / sizeof(arr[0]);
     // selection_sort(arr, size);
     // selection_sort_desc(arr, size);
-    selectSort(arr, size);
+    // selectSort(arr, size);
+
+    char ch[7] = {'d', 'c', 'a', 'f', 'z', 'x', 't'};
+    int n = sizeof(ch)/sizeof(ch[0]);
+    selectSortCharacter(ch, n);
     return 0;
 }
