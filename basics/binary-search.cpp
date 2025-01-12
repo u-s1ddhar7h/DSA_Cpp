433080dc921dc8f3ce971b5e1b7529bf4c5ff419
Siddharth <178361631+u-s1ddhar7h@users.noreply.github.com>
2025-01-12 22:31:30 +0530

    square root of a number

--- basics/binary-search.cpp
+++ basics/binary-search.cpp
@@ -56,12 +56,26 @@ int insertPosition(int arr[], int n, int target) {
     }
     return index;
 }
+// Square Root of a number
+int sqRoot(int num) {
+    int start = 1, end = num, mid, ans;
+    while (start <= end) {
+        mid = start + (end - start) / 2;
+        if (mid * mid == num) return mid;
+        else if (mid * mid < num) {
+            ans = mid;
+            start = mid + 1;
+        } else end = mid - 1;
+    }
+    return ans;
+}
 int main() {
     int arr[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
     int n = sizeof(arr) / sizeof(arr[0]);
     int key = 30;
     // cout << binarySearch(arr, n, key) << endl;
     // cout << firstAndLastPos(arr, n, key) << endl;
-    cout << insertPosition(arr, n, key) << endl;
+    // cout << insertPosition(arr, n, key) << endl;
+    cout << sqRoot(49) << endl;
     return 0;
 }
