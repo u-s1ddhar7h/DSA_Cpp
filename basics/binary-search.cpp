802f6ff28c2d8e1932c03c971a2e5a80a94f9ad2
Siddharth <178361631+u-s1ddhar7h@users.noreply.github.com>
2025-01-13 16:06:48 +0530

    find total occurrence and find peak in bitonic array

--- basics/binary-search.cpp
+++ basics/binary-search.cpp
@@ -69,13 +69,59 @@ int sqRoot(int num) {
     }
     return ans;
 }
+// number of occurrence
+int totalOccurrence(int arr[], int n, int target) {
+    int first = -1, last = -1, start = 0, end = n - 1, mid;
+    // first occurrence
+    while (start <= end) {
+        mid = start + (end - start) / 2;
+        if (arr[mid] == target) {
+            first = mid;
+            end = mid - 1;
+        } else if (arr[mid] < target) start = mid + 1;
+        else end = mid - 1;
+    }
+    // last occurrence
+    start = first != -1 ? first : 0;
+    end = n - 1;
+    if (first != -1) {
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                last = mid;
+                start = mid + 1;
+            } else if (arr[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+    }
+    return last - first + 1;
+}
+int mountainPeak(int arr[], int n) {
+    int start = 0, end = n - 1;
+    int peak = -1;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] > arr[mid + 1]) {
+            peak = mid;
+            end = mid - 1;
+        } else start = mid + 1;
+    }
+    return peak;
+}
 int main() {
-    int arr[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    // int arr[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    // int arr[5] = {2, 4, 4, 4, 5};
+    int arr[7] = {2, 4, 6, 8, 10, 8, 5};
+    // int arr[3] = {0, 1, 0};
+    // int arr[5] = {3, 5, 3, 2, 0};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int key = 30;
+    int key = 4;
     // cout << binarySearch(arr, n, key) << endl;
     // cout << firstAndLastPos(arr, n, key) << endl;
     // cout << insertPosition(arr, n, key) << endl;
-    cout << sqRoot(49) << endl;
+    // cout << sqRoot(49) << endl;
+    // cout << totalOccurrence(arr, n, key) << endl;
+    cout << mountainPeak(arr, n) << endl;
+    // cout << -2 / 2 << endl;
     return 0;
 }
