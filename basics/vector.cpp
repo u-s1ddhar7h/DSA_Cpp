0e9c3766c1459dcbeec6c5bf16c21618de194f57
Siddharth <178361631+u-s1ddhar7h@users.noreply.github.com>
2025-01-19 22:37:46 +0530

    vector initialization

--- basics/vector.cpp
+++ basics/vector.cpp
@@ -6,9 +6,9 @@ using std::endl;
 using std::vector;
 
 int main() {
-
-    // Declare vector
     vector<int> v;
+    vector<int> v1(5, 1);
+    vector<int> v2 = {1, 2, 3, 4, 5};
     cout << "Size of vector 'V': " << v.size() << endl;
     cout << "Capacity of vector 'V': " << v.capacity() << endl;
     v.push_back(2);
@@ -16,5 +16,18 @@ int main() {
     v.push_back(10);
     cout << "Size of vector 'V': " << v.size() << endl;
     cout << "Capacity of vector 'V': " << v.capacity() << endl;
+
+    cout << "Size of vector 'V1': " << v1.size() << endl;
+    cout << "Capacity of vector 'V1': " << v1.capacity() << endl;
+    v1.push_back(8);
+    cout << "Size of vector 'V1': " << v1.size() << endl;
+    cout << "Capacity of vector 'V1': " << v1.capacity() << endl;
+
+    cout << "Size of vector 'V2': " << v2.size() << endl;
+    cout << "Capacity of vector 'V2': " << v2.capacity() << endl;
+
+    // Update value in vector
+    v[1] = 5;
+
     return 0;
 }
