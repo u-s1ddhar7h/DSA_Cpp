4de522f124cdf5340476d149bb1f113887117063
Siddharth <178361631+u-s1ddhar7h@users.noreply.github.com>
2025-01-21 21:24:03 +0530

    two sum

--- basics/two-pointers.cpp
+++ basics/two-pointers.cpp
@@ -24,9 +24,49 @@ void segregateZeroOne(int arr[], int n) {
     printArray(arr, n);
 }
 
+void twoSum(int arr[], int n, int target) {
+    // O(N^2): Brute force approach
+    // for (int i = 0; i < n - 1; i++) {
+    //     for (int j = i + 1; j < n; j++) {
+    //         if (arr[i] + arr[j] == target) {
+    //             cout << "a = " << arr[i] << "\nb = " << arr[j] << endl;
+    //             return;
+    //         }
+    //     }
+    // }
+
+    // O(NlogN): binary search approach
+    // for (int i = 0; i < n - 1; i++) {
+    //     int x = target - arr[i];
+    //     int start = i + 1, end = n - 1;
+    //     while (start <= end) {
+    //         int mid = start + (end - start) / 2;
+    //         if (arr[mid] == x) {
+    //             cout << "a = " << arr[i] << "\nb = " << arr[mid] << endl;
+    //             return;
+    //         } else if (arr[mid] < x) start = mid + 1;
+    //         else end = mid - 1;
+    //     }
+    // }
+
+    // O(N): tow pointer approach
+    int start = 0, end = n - 1;
+    while (start < end) {
+        if (arr[start] + arr[end] == target) {
+            cout << "a: " << arr[start] << "\nb: " << arr[end] << endl;
+            break;
+        }
+        else if (arr[start] + arr[end] < target) start++;
+        else end--;
+    }
+}
+
 int main() {
     int arr[] = {1, 0, 1, 0, 1, 0};
+    int arr[] = {2, 7, 11, 15, 27};
+    int arr[] = {1, 2, 4, 6, 18, 25};
     int n = sizeof(arr) / sizeof(arr[0]);
     segregateZeroOne(arr, n);
+    twoSum(arr, n, 10);
     return 0;
 }
