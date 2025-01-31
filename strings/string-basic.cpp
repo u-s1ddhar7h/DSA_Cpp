68747c61b42fdba1f82f5fe7fa65369a2c1f8e31
Siddharth <178361631+u-s1ddhar7h@users.noreply.github.com>
2025-01-31 21:50:26 +0530

    reverse a string

--- strings/string-basic.cpp
+++ strings/string-basic.cpp
@@ -4,6 +4,15 @@ using std::cout;
 using std::endl;
 using std::string;
 
+void stringReverse(string str) {
+    int start = 0, end = str.size() - 1;
+    while (start < end) {
+        std::swap(str[start], str[end]);
+        start++, end--;
+    }
+    cout << str << endl;
+}
+
 int main() {
     // Initialize string
     string s = "John";
@@ -21,5 +30,6 @@ int main() {
     string s4 = "Rohit is a \"good\" boy";
     cout << s4 << endl;
     cout << "\\0" << endl;
+    stringReverse("Rohit");
     return 0;
 }
