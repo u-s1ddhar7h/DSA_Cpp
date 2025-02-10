940305d3f0c1e0a94fa79dfc04828f2de051c582
Siddharth <178361631+u-s1ddhar7h@users.noreply.github.com>
2025-02-10 19:55:07 +0530

    integer to roman

--- strings/string-basic.cpp
+++ strings/string-basic.cpp
@@ -272,6 +272,79 @@ int romanToIntegers(const string& s) {
     return sum;
 }
 
+// unit-place
+string unitPlaceString(int n) {
+    switch (n) {
+        case 1: return "I";
+        case 2: return "II";
+        case 3: return "III";
+        case 4: return "IV";
+        case 5: return "V";
+        case 6: return "VI";
+        case 7: return "VII";
+        case 8: return "VIII";
+        case 9: return "IX";
+    }
+    return "";
+}
+// tens-place
+string tensPlaceString(int n) {
+    switch (n) {
+        case 1: return "X";
+        case 2: return "XX";
+        case 3: return "XXX";
+        case 4: return "XL";
+        case 5: return "L";
+        case 6: return "LX";
+        case 7: return "LXX";
+        case 8: return "LXXX";
+        case 9: return "XC";
+    }
+    return "";
+}
+// hundreds-place
+string hundredsPlaceString(int n) {
+    switch (n) {
+        case 1: return "C";
+        case 2: return "CC";
+        case 3: return "CCC";
+        case 4: return "CD";
+        case 5: return "D";
+        case 6: return "DC";
+        case 7: return "DCC";
+        case 8: return "DCCC";
+        case 9: return "CM";
+    }
+    return "";
+}
+// thousands-place
+string thousandsPlaceString(int n) {
+    switch (n) {
+        case 1: return "M";
+        case 2: return "MM";
+        case 3: return "MMM";
+    }
+    return "";
+}
+void intToRoman(int num) {
+    // TC: O(1) & SC: O(1)
+    string ans;
+    if (num >= 1000) {
+        ans += thousandsPlaceString(num/1000);
+        num %= 1000;
+    }
+    if (num >= 100) {
+        ans += hundredsPlaceString(num/100);
+        num %= 100;
+    }
+    if (num >= 10) {
+        ans += tensPlaceString(num/10);
+        num %= 10;
+    }
+    ans += unitPlaceString(num);
+    cout << ans << endl;
+}
+
 int main() {
     // Initialize string
     // string s = "John";
@@ -316,7 +389,9 @@ int main() {
     // if (num1.size() < num2.size())
     //     cout << addStringInteger(num2, num1) << endl;
     // else cout << addStringInteger(num1, num2) << endl;
-    string s = "MCCXLVIII";
-    cout << romanToIntegers(s);
+    // string s = "MCCXLVIII";
+    // cout << romanToIntegers(s) << endl;
+    int num = 3749;
+    intToRoman(num);
     return 0;
 }
